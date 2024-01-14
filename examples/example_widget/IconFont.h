5eedb38f8fd17d7232f39f6f89b094f3ad4c9ef5
onecodeworld <onecodeworld@163.com>
2024-01-14 22:30:13 +0800

    add iconfont

---
+++ examples/example_widget/IconFont.h
@@ -0,0 +1,12 @@
+#ifndef ICONFONT_H
+#define ICONFONT_H
+
+enum FontStyle { kCustomIconFont = 1 };
+
+#define FONT_CLOSE    0xe903
+#define FONT_MINIMIZE 0xe629
+#define FONT_MAXIMIZE 0xe65b
+#define FONT_RESTORE  0xe673
+#define FONT_HELP     0xe619
+
+#endif // ICONFONT_H
